bfff3ca0387bba9a24d5694d0e6d1f00f9da3d97
kodaneflash <jamesonmarcher@gmail.com>
2024-01-18 19:23:52 -0500

    modified:   src/app/notes/NavBar.tsx 	modified:   src/app/page.tsx 	modified:   src/app/sign-in/[[...sign-in]]/page.tsx

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "FlowBrain - Sign In",
+  title: "Syncade - Sign In",
 };
 
 export default function SignInPage() {
