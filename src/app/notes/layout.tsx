44f0660abc5ed8d9c6a7e777790c511f711374f9
kodaneflash <jamesonmarcher@gmail.com>
2024-01-18 22:32:35 -0500

    modified:   src/app/notes/layout.tsx

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -1,4 +1,20 @@
 import NavBar from "./NavBar";
+import type { Metadata } from "next"; // Import Metadata type if needed
+
+export const metadata: Metadata = {
+  title: 'Syncade',
+  description: 'Get a Second Brain with Syncade, an intelligent note-taking app powered by AI',
+  openGraph: {
+    title: 'Syncade',
+    description: 'Get a Second Brain with Syncade, an intelligent note-taking app powered by AI',
+    images: ['/opengraph-image.png'],
+    url: 'https://www.syncade.io/',
+    type: 'website',
+  },
+  metadataBase: new URL(process.env.NODE_ENV === 'production' ? 'https://www.s.pro' : 'http://localhost:3000'),
+  // Additional metadata options
+};
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
